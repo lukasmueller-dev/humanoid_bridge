003def265ac415333a17a13d7d86aec0e534f25c
xuanhao <1834831158@qq.com>
2025-09-23 16:47:21 +0200

    Update source script

--- setup_unitree.sh
+++ setup_unitree.sh
@@ -1,7 +1,12 @@
-source /opt/ros/humble/setup.bash
-source $HOME/sairol_ws/install/setup.bash
+if [ "$USER" != "unitree" ]; then
+    echo "Source /opt/ros/humble/setup.bash and set RMW_IMPLEMENTATION and CYCLONEDDS_URI"
+    source /opt/ros/humble/setup.bash
+    export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+    export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
+                                <NetworkInterface name="enp4s0" priority="default" multicast="default" />
+                            </Interfaces></General></Domain></CycloneDDS>'
+fi
 
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
-                            <NetworkInterface name="enp4s0" priority="default" multicast="default" />
-                        </Interfaces></General></Domain></CycloneDDS>'
+if [ -z "$ROS_DISTRO" ]; then
+    echo "ROS_DISTRO is not set. Please source ros2"
+fi
